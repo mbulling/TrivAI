45f0e80d85f4a97b5814dceee0df4d23e1098969
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-02-06 11:28:38 -0500

    Initial Commit

---
+++ TrivAI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TrivAI
+//
+//  Created by Mason Bulling on 2/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrivAI/TrivAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivAIApp.swift
+//  TrivAI
+//
+//  Created by Mason Bulling on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TrivAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
