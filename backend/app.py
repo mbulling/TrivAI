4a65e4f9bfa527fa42860a4558b6e6eb3a2d39c4
JamesZhang2 <jameszhang2019@outlook.com>
2023-02-24 18:56:23 -0500

    Create endpoints for MCQs and T/F Questions

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from Questgen import main
 from flask import Flask, render_template, request
 import random
 import nltk
+import json
 nltk.download('stopwords')  # ???
 
 app = Flask(__name__)
@@ -15,7 +16,7 @@ def index():
         user_input = request.form['user_input']
 
         # Generate four multiple choice questions
-        questions = generate_questions(user_input)
+        questions = generate_mcqs(user_input)
 
         # Render the questions template with the questions and user input
         return render_template('questions.html',
@@ -25,7 +26,25 @@ def index():
         return render_template('index.html')
 
 
-def generate_questions(user_input):
+@app.route('/mcq/', methods=['POST'])
+def get_mcq():
+    ''' Create multiple choice questions '''
+    body = json.loads(request.data)
+    user_input = body["user_input"]
+    questions = generate_mcqs(user_input)
+    return json.dumps(questions)
+
+
+@app.route('/tf/', methods=['POST'])
+def get_tf():
+    ''' Create a True/False questions '''
+    body = json.loads(request.data)
+    user_input = body["user_input"]
+    questions = generate_tfs(user_input)
+    return json.dumps(questions)
+
+
+def generate_mcqs(user_input):
     # Generate num_questions random questions based on the user input
     question_list = []
     qg = main.QGen()
@@ -45,11 +64,19 @@ def generate_questions(user_input):
         question = {'text': question_statement,
                     'options_sol': options_sol, 'answer': answer}
         question_list.append(question)
-    # for i in range(3):
-    #     # Dummy example: generate random answers based on user input
-    #     answers = [user_input + str(i) for i in range(4)]
-    #     random.shuffle(answers)
-    #     question = {'text': f"What is {user_input}?", 'answers': answers}
-    #     question_list.append(question)
     print(question_list)
+    return question_list
+
+
+def generate_tfs(user_input):
+    # Generate num_questions random questions based on the user input
+    question_list = []
+    qg = main.BoolQGen()
+    payload = {
+        "input_text": user_input
+    }
+    output = qg.predict_boolq(payload)
+    for q in output["Boolean Questions"]:
+        print(q)
+        question_list.append(q)
     return question_list
