5b4c010215e5060bbcd41d02ef70ac8c5f49d91e
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-03-29 11:12:33 -0400

    Functioning TopicList

--- trivai/main/TopicList.swift
+++ trivai/main/TopicList.swift
@@ -13,9 +13,11 @@ import Alamofire
 struct TopicList: View {
     /// - View Properties
     @State private var topicList: [String] = []
+    @State private var quizInfo: Info = Info(title: "Testing", peopleAttended: 100, rules: ["Answer the questions carefully"])
+    @State private var questionListWrapper: QuestionListWrapper? = nil
     
     func getTopics() {
-        AF.request("http://100.26.175.163:5000/topics").responseJSON { response in
+        AF.request("http://127.0.0.1:5000/topics").responseJSON { response in
             print(response.result)
             switch response.result {
             case .success(let value):
@@ -38,23 +40,59 @@ struct TopicList: View {
         VStack(alignment: .leading) {
             if !topicList.isEmpty {
                 Text("Your Quizzes")
-                    .font(.system(size: 30))
+                    .font(.system(size: 40))
                     .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
                     .padding(.leading, 30)
                     .offset(y: 20)
             }
             
-            ScrollView(.horizontal, showsIndicators: false) {
-                ForEach(topicList, id: \.self) { topic in
-                    Text(topic)
-                }
+            
+                NavigationView {
+                    ScrollView(.vertical, showsIndicators: false) {
+                    ForEach(topicList, id: \.self) { topic in
+                        Button (action: {
+                            NetworkManager.createTopicQuestion(topic: topic) { questions, success, error in
+                                if (success) {
+                                    DispatchQueue.main.async {
+                                        self.questionListWrapper = QuestionListWrapper(questions: questions ?? [])
+                                    }
+                                } else {
+                                    print("Error decoding question")
+                                }
+                            }
+                        }) {
+                            HStack {
+                                Text(topic)
+                                    .font(.system(size:30))
+                                    .bold()
+                                    .foregroundColor(Color.white)
+                                    .frame(maxWidth: .infinity)
+                            }
+                            .padding()
+                            .background(Color("background3"))
+                            .cornerRadius(80)
+                        }.sheet(item: $questionListWrapper) { wrapper in
+                            QuestionsView(info: quizInfo, questions: wrapper.questions) {
+                                // some action
+                            }
+                        }
+                    }
+                }.background(Color.blue)
+                        
             }
             .padding(20)
-            .padding(.leading, 10)
         }
+        .background(Color.blue)
         .onAppear {
             self.getTopics()
         }
     }
 }
 
+
+struct Previews_TopicList_Previews: PreviewProvider {
+    static var previews: some View {
+        TopicList()
+    }
+}
