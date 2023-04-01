6d7547c783c66d9da4f1ccbb3b2777f5797a7d24
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-04-01 18:13:34 -0400

    Updated topics page

--- trivai/main/TopicList.swift
+++ trivai/main/TopicList.swift
@@ -17,7 +17,7 @@ struct TopicList: View {
     @State private var questionListWrapper: QuestionListWrapper? = nil
     
     func getTopics() {
-        AF.request("http://10.48.21.136:3000/topics").responseJSON { response in
+        AF.request("http://192.168.4.43:3000/topics").responseJSON { response in
             print(response.result)
             switch response.result {
             case .success(let value):
@@ -42,48 +42,85 @@ struct TopicList: View {
                 Text("Your Quizzes")
                     .font(.system(size: 40))
                     .fontWeight(.heavy)
-                    .foregroundColor(Color.white)
+                    .foregroundColor(Color.black)
                     .padding(.leading, 30)
                     .offset(y: 20)
             }
-            
-            
-                NavigationView {
-                    ScrollView(.vertical, showsIndicators: false) {
-                    ForEach(topicList, id: \.self) { topic in
-                        Button (action: {
-                            NetworkManager.createTopicQuestion(topic: topic, num_questions: 3) { questions, success, error in
-                                if (success) {
-                                    DispatchQueue.main.async {
-                                        self.questionListWrapper = QuestionListWrapper(questions: questions ?? [])
+            NavigationView {
+               List {
+                   ForEach(topicList, id: \.self) { topic in
+                        HStack(spacing: 12.0) {
+//                           Image(item.image)
+//                              .resizable()
+//                              .aspectRatio(contentMode: .fit)
+//                              .frame(width: 80, height: 80)
+//                              .background(Color("background"))
+//                              .cornerRadius(20)
+                            Button (action: {
+                                NetworkManager.createTopicQuestion(topic: topic, num_questions: 3) { questions, success, error in
+                                    if (success) {
+                                        DispatchQueue.main.async {
+                                            self.questionListWrapper = QuestionListWrapper(questions: questions ?? [])
+                                        }
+                                    } else {
+                                        print("Error decoding question")
                                     }
-                                } else {
-                                    print("Error decoding question")
+                                }
+                            }) {
+                                VStack(alignment: .leading) {
+                                    HStack {
+                                        Text(topic)
+                                           .font(.system(size: 30))
+                                         
+                                         Spacer()
+                                        
+                                         Text("HARD")
+                                            .lineLimit(2)
+                                            .lineSpacing(4)
+                                            .font(.subheadline)
+                                            .frame(height: 50.0)
+                                    }
+                                   
+
+                                    Text("3 QUESTIONS")
+                                       .font(.caption)
+                                       .fontWeight(.bold)
+                                       .foregroundColor(.gray)
+                                    
+        
+                                .cornerRadius(80)
+                            }.sheet(item: $questionListWrapper) { wrapper in
+                                QuestionsView(info: quizInfo, questions: wrapper.questions) {
+                                    // some action
                                 }
                             }
-                        }) {
-                            HStack {
-                                Text(topic)
-                                    .font(.system(size:30))
-                                    .bold()
-                                    .foregroundColor(Color.white)
-                                    .frame(maxWidth: .infinity)
-                            }
-                            .padding()
-                            .background(Color("background3"))
-                            .cornerRadius(80)
-                        }.sheet(item: $questionListWrapper) { wrapper in
-                            QuestionsView(info: quizInfo, questions: wrapper.questions) {
-                                // some action
-                            }
+
+                           
+
+                              
+                           }
                         }
-                    }
-                }.background(Color.blue)
-                        
+                     }
+                     .padding(.vertical, 8.0)
+                  }
+                  //.onDelete { index in
+                     //self.store.updates.remove(at: index.first!)
+                  //}
+                  //.onMove(perform: move)
+               }
+               //.navigationBarTitle(Text("Your Quizzes"))
+               .navigationBarItems(
+                  //leading: Button(action: addUpdate) { Text("Add Update") },
+                  trailing: EditButton()
+               )
             }
-            .padding(20)
-        }
-        .background(Color.blue)
+
+            
+            
+        
+                        
+           
+        
         .onAppear {
             self.getTopics()
         }
