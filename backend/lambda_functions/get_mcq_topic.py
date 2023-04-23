5b7eac963410a371d067b2fe1f0d155f4c765cec
JamesZhang2 <jameszhang2019@outlook.com>
2023-04-23 09:20:00 -0400

    Fix get_mcq_topic

--- backend/lambda_functions/get_mcq_topic.py
+++ backend/lambda_functions/get_mcq_topic.py
@@ -23,14 +23,18 @@ def lambda_handler(event, context):
     ''' [event] contains the following keys:
         - topic: subject to generate questions for
         - num_questions:  number of questions to generate
+
+        Returns a list of dictionaries representing the list of questions,
+        or {'statusCode': 400, 'body': {'error': 'msg'}} if there is an error
     '''
+
     try:
         topic = event["topic"]
         num_questions = event["num_questions"]
         if num_questions > MAX_QUESTIONS:  # FIXME: cap instead of error?
             return {
                 'statusCode': 400,  # FIXME: should have different error code?
-                'body': json.dumps({"error": "Cannot generate more than %d questions at a time." % MAX_QUESTIONS})
+                'body': {"error": "Cannot generate more than %d questions at a time." % MAX_QUESTIONS}
             }
 
         res = ""
@@ -43,7 +47,8 @@ def lambda_handler(event, context):
                 # Generate more questions and put the updated list of questions into the database
                 status, more_questions = mcq_topic(
                     topic, num_questions - len(questions))
-                questions = questions + json.loads(more_questions)
+                questions = questions + \
+                    json.loads(more_questions)  # list of dicts
                 response = questions_table.update_item(
                     Key=key,
                     UpdateExpression="set questions=:q",
@@ -53,10 +58,11 @@ def lambda_handler(event, context):
             else:
                 # Randomly choose num_questions questions to return
                 questions = random.sample(questions, num_questions)
-            return {
-                'statusCode': 200,
-                'body': json.dumps(questions)
-            }
+            return questions
+            # return {
+            #     'statusCode': 200,
+            #     'body': questions
+            # }
         else:
             status, res = mcq_topic(topic, num_questions)
             # store if not error
@@ -64,14 +70,15 @@ def lambda_handler(event, context):
                 item = {"topic_id": topic, "questions": res}
                 print(item)
                 questions_table.put_item(Item=item)
-                return {
-                    'statusCode': 200,
-                    'body': res
-                }
+                return json.loads(res)
+                # return {
+                #     'statusCode': 200,
+                #     'body': json.loads(res)
+                # }
     except Exception as e:
         return {
             'statusCode': 400,
-            'body': json.dumps({"error": e})
+            'body': {"error": e}
         }
 
 
@@ -102,10 +109,9 @@ def get_chatgpt(prompt):  # (int, str)
         messages=[{"role": "user",
                    "content": prompt}]
     )
-    content = completion["choices"][0]["message"]["content"]
+    content = completion["choices"][0]["message"]["content"]  # str
 
     try:
-        json.loads(content)
         return 200, content
     except:
         return 400, json.dumps({"error": "OpenAI did not return a json string"})
